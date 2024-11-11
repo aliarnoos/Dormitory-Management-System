8ed5eb1cf89a44bed62d9f4250ca4a11af26a2da
aliarnoos <alimarnoos@gmai.com>
2024-11-11 18:45:02 +0300

    fix bug

--- app/Filament/Resources/ApartmentResource.php
+++ app/Filament/Resources/ApartmentResource.php
@@ -12,26 +12,65 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
 
 class ApartmentResource extends Resource
 {
-    protected static ?string $model = User::class;
+    protected static ?string $model = Apartment::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-building-office';
 
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                //
-            ]);
+        ->schema([
+            TextInput::make('id')
+                ->disabled()
+                ->label('#ID'),
+
+            Select::make('apartment_type')
+                ->label('Apartment Type')
+                ->options([
+                    'standard' => 'Standard',
+                    'private' => 'Private',
+                    'economy' => 'Economy',
+                ])
+                ->required(),
+
+            TextInput::make('floor')
+                ->label('Floor')
+                ->numeric()
+                ->required(),
+
+            TextInput::make('number')
+                ->label('Room Number')
+                ->numeric()
+                ->required(),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->alignCenter()
+                    ->label('`#ID'),
+
+                TextColumn::make('apartment_type')
+                    ->alignCenter()
+                    ->label('Apartment Type'),
+                
+                TextColumn::make('floor')
+                    ->alignCenter()
+                    ->label('`Floor'),
+
+                TextColumn::make('number')
+                    ->alignCenter()
+                    ->label('Room Number'),
+
             ])
             ->filters([
                 //
@@ -57,8 +96,8 @@ class ApartmentResource extends Resource
     {
         return [
             'index' => Pages\ListApartments::route('/'),
-            'create' => Pages\CreateApartment::route('/create'),
-            'edit' => Pages\EditApartment::route('/{record}/edit'),
+            // 'create' => Pages\CreateApartment::route('/create'),
+            // 'edit' => Pages\EditApartment::route('/{record}/edit'),
         ];
     }
 }
